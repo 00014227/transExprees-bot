5dce865243eaf0ab03b08d90e6654f953073d920
Ahmad <tulkinxojaevaxad@gmail.com>
2025-08-21 14:56:21 +0500

    display object

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ function App() {
     if (window.Telegram?.WebApp) {
       const tg = window.Telegram.WebApp;
       console.log("User:", tg.initDataUnsafe.user);
-      alert(tg, tg.initDataUnsafe.user)
+  
+      alert(JSON.stringify(tg.initDataUnsafe.user, null, 2)); // formatted
     } else {
       console.log("Telegram WebApp API not available");
-      alert("Telegram WebApp API not available")
+      alert("Telegram WebApp API not available");
     }
   }, []);
   
