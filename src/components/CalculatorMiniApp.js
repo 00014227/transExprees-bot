a06a625284ca04af40596c70bb41ea229005d21f
Ahmad <tulkinxojaevaxad@gmail.com>
2025-08-21 09:59:11 +0500

    add script tg

--- src/components/CalculatorMiniApp.js
+++ src/components/CalculatorMiniApp.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function CalculatorMiniApp() {
   const tg = window.Telegram.WebApp;
-  const user = tg.initDataUnsafe.user; // данные пользователя
+  const user = tg.initDataUnsafe.user; 
 
   const [formData, setFormData] = useState({
     deliveryType: "",
